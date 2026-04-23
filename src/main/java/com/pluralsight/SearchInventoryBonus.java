c7e88590a7daf1cbd599d639f999774fcd44a693
polinabreha <polinabreha@gmail.com>
2026-04-23 08:21:26 -0400

    bonus

--- src/main/java/com/pluralsight/SearchInventoryBonus.java
+++ src/main/java/com/pluralsight/SearchInventoryBonus.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SearchInventoryBonus {
     public static void main(String[] args) {
@@ -19,25 +20,21 @@ public class SearchInventoryBonus {
 
 
     public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/inventory.csv"));
             String line;
+
             while ((line = bufferedReader.readLine()) != null) {
-                String[] values = line.split("//|");
+                String[] values = line.split("\\|");
                 Product product = new Product(Integer.parseInt(values[0]), values[1], Float.parseFloat(values[2]));
 
+                inventory.add(product);
             }
-
-
-
-
+            Collections.sort(inventory, (a, b) -> a.getName().compareTo(b.getName()));
         }catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("IOException in SearchInventoryBonus");
         }
-
-
-
-
-        return null;
+        return inventory;
     }
 }
